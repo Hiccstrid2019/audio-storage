48e54750c815aae721d850ceccb54ba8238b76ec
Hiccstrid2019 <hiccup.colin@yandex.ru>
2022-11-21 23:29:26 +0300

    init client react app

---
+++ react-ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
