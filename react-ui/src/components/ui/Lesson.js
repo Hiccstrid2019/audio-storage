e17db3b7dcda73f14933f8b084cc8cd57d6549e0
Hiccstrid2019 <hiccup.colin@yandex.ru>
2022-12-04 23:30:53 +0300

    change ui

--- react-ui/src/components/ui/Lesson.js
+++ react-ui/src/components/ui/Lesson.js
@@ -7,10 +7,10 @@ import Audio from "./Audio";
 const Lesson = ({lesson}) => {
     return (
        <Row justify='center' align='middle' style={{padding: "20px"}}>
-           <Card title={lesson.title} extra={<Link to={`${lesson.id}`}>More</Link>} style={{width: "40%"}}>
+           <Card title={lesson.title} extra={<Link to={`${lesson.id}`}>Open</Link>} style={{width: "40%"}}>
                {
                    lesson.audio.map((audio) => (
-                       <Audio audio={audio}/>
+                       <Audio  audio={audio}/>
                    ))
                }
            </Card>
