a4a5c718b4ffcaa0ab3f8aa72a49a22da2c0c644
Hiccstrid2019 <hiccup.colin@yandex.ru>
2022-12-06 23:32:06 +0300

    add controller for handle audio

---
+++ MusicProgress/Controllers/AudioController.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicProgress.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class AudioController : ControllerBase
+    {
+        [HttpPost("audio")]
+        public async Task<IActionResult> UploadAudio(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                await using (var stream = new FileStream("test.ogg", FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
