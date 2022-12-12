611795f31b94c82ec27a4a169872980c98ddf178
Hiccstrid2019 <hiccup.colin@yandex.ru>
2022-12-12 22:27:11 +0300

    refactor controller, now it used AudioService

--- MusicProgress/Controllers/AudioController.cs
+++ MusicProgress/Controllers/AudioController.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MusicProgress.Services;
+using MusicProgress.Services.Interfaces;
 
 namespace MusicProgress.Controllers
 {
@@ -9,18 +12,32 @@ namespace MusicProgress.Controllers
     [Route("api")]
     public class AudioController : ControllerBase
     {
+        private readonly IAudioService _audioService;
+
+        public AudioController(IAudioService audioService)
+        {
+            _audioService = audioService;
+        }
         [HttpPost("audio")]
         public async Task<IActionResult> UploadAudio(IFormFile file)
         {
             if (file.Length > 0)
             {
-                await using (var stream = new FileStream("test.ogg", FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
-                }
+                using var memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+                var id = await _audioService.UploadAudioAsync(memoryStream);
+                var url = await _audioService.GetUrlAudioAsync(id);
+                return Ok(new {UploadAudioId = id, Url = url});
             }
 
             return Ok();
+        }
+
+        [HttpGet("audio/{fileName}")]
+        public async Task<IActionResult> DownloadAudio(string fileName)
+        {
+            var stream = await _audioService.GetAudioAsync(fileName);
+            return File(stream, "application/octet-stream");
         }
     }
 }
