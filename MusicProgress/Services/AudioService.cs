c6bf7d8a6b88fc4b3d6ff553010e1c49918bc145
Hiccstrid2019 <hiccup.colin@yandex.ru>
2022-12-12 22:25:23 +0300

    added AudioService

---
+++ MusicProgress/Services/AudioService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using MusicProgress.Data;
+using MusicProgress.Services.Interfaces;
+
+namespace MusicProgress.Services;
+
+public class AudioService : IAudioService
+{
+    private readonly AppDbContext _context;
+    private readonly IFileAppService _fileAppService;
+
+    public AudioService(AppDbContext context, IFileAppService fileAppService)
+    {
+        _context = context;
+        _fileAppService = fileAppService;
+    }
+    
+    public async Task<string> UploadAudioAsync(Stream stream)
+    {
+        var id = Guid.NewGuid() + ".ogg";
+        await _fileAppService.UploadObjectAsync(id, stream);
+        return id;
+    }
+
+    public async Task<Stream> GetAudioAsync(string audioId)
+    {
+        var stream = await _fileAppService.GetObjectAsync(audioId);
+        return stream;
+    }
+
+    public async Task<string> GetUrlAudioAsync(string audioId)
+    {
+        var url = await _fileAppService.GetUrlObjectAsync(audioId);
+        return url;
+    }
+
+    public Task<List<string>> GetAudioIdsByUserIdAsync(int userId)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ MusicProgress/Services/Interfaces/IAudioService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MusicProgress.Services.Interfaces;
+
+public interface IAudioService
+{
+    Task<string> UploadAudioAsync(Stream stream);
+    Task<Stream> GetAudioAsync(string audioId);
+    Task<string> GetUrlAudioAsync(string audioId);
+    Task<List<string>> GetAudioIdsByUserIdAsync(int userId);
+}
