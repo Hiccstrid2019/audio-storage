410fc5e1ae8822347e0f034bb4d87ea658eda848
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-01-05 23:09:39 +0300

    animation of the microphone icon when recording

--- react-ui/src/components/pages/LessonPage.js
+++ react-ui/src/components/pages/LessonPage.js
@@ -4,6 +4,17 @@ import {Context} from "../../index";
 import classes from "./LessonPage.module.css";
 import Audio from "../ui/Audio/Audio";
 import MicIcon from "./mic.svg"
+import MicIconFrame1 from "./mic1.svg"
+import MicIconFrame2 from "./mic2.svg"
+import MicIconFrame3 from "./mic3.svg"
+import Button from "../ui/Button/Button";
+
+const icons = [
+    MicIcon,
+    MicIconFrame1,
+    MicIconFrame2,
+    MicIconFrame3,
+]
 
 const LessonPage = () => {
     const {store} = useContext(Context);
@@ -17,30 +28,53 @@ const LessonPage = () => {
     const {id} = useParams();
 
     const [chunks, setChunks] = useState([]);
-    const [record, setRecord] = useState(false);
+    const [start, setStart] = useState(false);
+    const [recording, setRecording] = useState(false);
     const [audio, setAudio] = useState(null);
+    const [index, setIndex] = useState(0);
+    const [{timer}, setTimer] = useState({timer: null});
 
     const handleRecord = () => {
-        if (!mediaRecorder.current) {
-            navigator.mediaDevices.getUserMedia({audio: true})
-                .then((stream) => {
-                    mediaRecorder.current = new MediaRecorder(stream);
-                    mediaRecorder.current.start();
-                    setRecord(true);
-                    mediaRecorder.current.onstop = stopRecording;
-                    mediaRecorder.current.ondataavailable = (e) => {
-                        chunks.push(e.data);
-                        setChunks(chunks => [...chunks, e.data]);
-                    };
-                })
-                .catch((err) => {
-                    console.error(err);
-                })
+        setRecording(recording => !recording);
+        if (timer == null) {
+            let newTimer = setInterval(() => {
+                setIndex(index => index + 1);
+            }, 500);
+            setTimer(() => ({timer: newTimer}));
         } else {
-            mediaRecorder.current.stop();
-            mediaRecorder.current = null;
-            setRecord(false);
+            setTimer(prevState => {
+                let {timer} = prevState;
+                clearInterval(timer);
+                setIndex(0);
+                return {timer: null};
+            })
         }
+
+        // setInterval(() => {
+        //     console.log("frame");
+        //     console.log("index: "+ index)
+        //     // setIcon(icons[index % 2]);
+        //     setIndex(index => index + 1);
+        // }, 1000)
+        // if (!mediaRecorder.current) {
+        //     navigator.mediaDevices.getUserMedia({audio: true})
+        //         .then((stream) => {
+        //             mediaRecorder.current = new MediaRecorder(stream);
+        //             mediaRecorder.current.start();
+        //             mediaRecorder.current.onstop = stopRecording;
+        //             mediaRecorder.current.ondataavailable = (e) => {
+        //                 chunks.push(e.data);
+        //                 setChunks(chunks => [...chunks, e.data]);
+        //             };
+        //         })
+        //         .catch((err) => {
+        //             console.error(err);
+        //         })
+        // } else {
+        //     mediaRecorder.current.stop();
+        //     mediaRecorder.current = null;
+        //     setRecording(false);
+        // }
     }
 
     const stopRecording = (e) => {
@@ -55,9 +89,19 @@ const LessonPage = () => {
         <div className={classes.container}>
             <div className={classes.title}>{store.lessons.find(lesson => lesson.id === Number(id)).title}</div>
             {
-                store.lessons.find(lesson => lesson.id === Number(id)).audio.map(audio => <Audio key={audio.id} audioUrl={audio.url}/>)
+                // store.lessons.find(lesson => lesson.id === Number(id)).audio.map(audio => <Audio key={audio.id} audioUrl={audio.url}/>)
             }
-            <img src={MicIcon}/>
+            <div className={classes.new}>
+                {!start ? <Button text="Add new record" onClick={() => setStart(true)}/> :
+                    <>
+                        Click mic to {!recording ? 'start' : 'stop'} recording
+                        <div className={classes.iconHolder}>
+                            <img src={icons[index % icons.length]}
+                                 className={classes.icon}
+                                 onClick={handleRecord}/>
+                        </div>
+                    </>}
+            </div>
         </div>
     );
 };
