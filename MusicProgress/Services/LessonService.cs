911dee2ea70fb066b7b0c4ab6ebad1a3062a8464
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-01-08 20:54:13 +0300

    added lesson service

---
+++ MusicProgress/Services/LessonService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using MusicProgress.Data;
+using MusicProgress.Services.Interfaces;
+
+namespace MusicProgress.Services;
+
+public class LessonService : ILessonService
+{
+    private readonly AppDbContext _context;
+
+    public LessonService(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<int> CreateLessonAsync(Lesson lesson)
+    {
+        await _context.Lessons.AddAsync(lesson);
+        await _context.SaveChangesAsync();
+        return lesson.LessonId;
+    }
+}
