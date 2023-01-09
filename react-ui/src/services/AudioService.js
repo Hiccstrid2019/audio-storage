55c9137d414003cb661beb796df32b72a2a7b45f
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-01-09 22:34:33 +0300

    now the audio is added to the lesson (in db)

--- react-ui/src/services/AudioService.js
+++ react-ui/src/services/AudioService.js
@@ -1,9 +1,10 @@
 import api from "../http";
 
 export default class AudioService {
-    static async saveFile(blob) {
+    static async saveFile(blob, lessonId) {
         const data = new FormData();
-        data.append('file', blob);
+        data.append('audioFile', blob);
+        data.append("lessonId", lessonId)
         return api.post('audio', data, {
             headers: {'Content-Type': `multipart/form-data`}
         });
