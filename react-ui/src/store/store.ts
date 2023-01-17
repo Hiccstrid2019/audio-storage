f0df3aeaaf4cced1f7d55ad6befb24d552d4a388
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-01-17 20:38:46 +0300

    switched to the redux

--- react-ui/src/store/store.ts
+++ react-ui/src/store/store.ts
@@ -13,6 +13,6 @@ export const setupStore = () => {
     })
 }
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore['dispatch'];
