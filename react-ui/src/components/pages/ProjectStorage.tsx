b3197ebf959ad8b2c0ea243a43ee92d5e0488104
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-01-29 23:54:00 +0300

    made the modal window a reusable component

--- react-ui/src/components/pages/ProjectStorage.tsx
+++ react-ui/src/components/pages/ProjectStorage.tsx
@@ -3,7 +3,9 @@ import Card from "../ui/Card/Card";
 import classes from "./ProjectStorage.module.css";
 import Modal from "../ui/Modal/Modal";
 import {useAppDispatch, useAppSelector} from "../../hoc/redux";
-import {fetchProjects} from "../../store/reducers/ProjectActions";
+import {addProject, fetchProjects} from "../../store/reducers/ProjectActions";
+import Input from "../ui/Input/Input";
+import Button from "../ui/Button/Button";
 
 const ProjectStorage = () => {
     const [active, setActive] = useState(false);
@@ -14,6 +16,13 @@ const ProjectStorage = () => {
         dispatch(fetchProjects());
     }, [])
 
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const handleForm = () => {
+        setActive(false);
+        dispatch(addProject({title, category}));
+    }
+
     return (
         <div className={classes.container}>
             {
@@ -22,7 +31,17 @@ const ProjectStorage = () => {
             <div className={classes.newLesson} onClick={() => setActive(true)}>
                 Add Project
             </div>
-            {active && <Modal active={active} setActive={setActive}/>}
+            {active &&
+                <Modal setActive={setActive}>
+                    <div className={classes.title}>Create new project</div>
+                    <Input text="Enter project name" setValue={setTitle}/>
+                    <Input text="Enter category name" setValue={setCategory}/>
+                    <div className={classes.btnRow}>
+                        <Button text='Cancel' style={{marginRight: "8px"}} onClick={() => setActive(false)}/>
+                        <Button text='Add project' type='primary' onClick={handleForm}/>
+                    </div>
+                </Modal>
+            }
         </div>
     );
 };
--- react-ui/src/components/ui/Modal/Modal.tsx
+++ react-ui/src/components/ui/Modal/Modal.tsx
@@ -1,34 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import classes from "./Modal.module.css";
-import Input from "../Input/Input";
-import Button from "../Button/Button";
-import {useAppDispatch} from "../../../hoc/redux";
-import {addProject} from "../../../store/reducers/ProjectActions";
 
 interface ModalProps {
-    active: boolean;
     setActive: React.Dispatch<boolean>;
+    children?: React.ReactNode;
 }
 
-const Modal = ({active, setActive}: ModalProps) => {
-    const [title, setTitle] = useState('');
-    const [category, setCategory] = useState('');
-    const dispatch = useAppDispatch();
-    const handleForm = () => {
-        // addLesson(name, category);
-        setActive(false);
-        dispatch(addProject({title, category}));
-    }
+const Modal = ({setActive, children}: ModalProps) => {
+
     return (
         <div className={classes.container} onClick={() => setActive(false)}>
             <div className={classes.content} onClick={e => e.stopPropagation()}>
-                <div className={classes.title}>Create new project</div>
-                <Input text="Enter project name" setValue={setTitle}/>
-                <Input text="Enter category name" setValue={setCategory}/>
-                <div className={classes.btnRow}>
-                    <Button text='Cancel' style={{marginRight: "8px"}} onClick={() => setActive(false)}/>
-                    <Button text='Add project' type='primary' onClick={handleForm}/>
-                </div>
+                {children}
             </div>
         </div>
     );
